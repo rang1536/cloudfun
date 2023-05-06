bc5dc8c47fe21d58c5e051e059b31e54a9ca6c62
user <user@DESKTOP-8K8IAI3>
2023-05-07 08:36:24 +0900

    .

--- cloudfun/src/main/java/com/cloudfun/www/HomeController.java
+++ cloudfun/src/main/java/com/cloudfun/www/HomeController.java
@@ -29,11 +29,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 
-	@Value("#{globals['upload.file.path']}") 
+
+	@Value("#{globals['upload.file.path']}")
     private String filePath;
-	
+
 
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -48,19 +48,19 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		// todo
         HttpSession session = request.getSession();
 		session.setAttribute("type", "text");
-		
+
 		if(session.getAttribute("localCountry") == null ) {
 			session.setAttribute("localCountry", request.getLocale().getCountry()); // KR
 		}
-				
+
 		//return "home";
 		return "mainPage";
 	}
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -78,8 +78,8 @@ public class HomeController {
 		//return "home";
 		return "component";
 	}
-	
-	
+
+
 	///terms
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -91,38 +91,44 @@ public class HomeController {
 		//return "home";
 		return "terms";
 	}
-	
-	// ÆÄÀÏ ´Ù¿î·Îµå Ã³¸®
+
+	// ï¿½ï¿½ï¿½ï¿½ ï¿½Ù¿ï¿½Îµï¿½ Ã³ï¿½ï¿½
     @RequestMapping("/fileDownload/{file}")
     public void fileDownload(@PathVariable String file,
                              HttpServletResponse response) throws IOException {
-    	
+
     	file = "60c1a4205d2545b6862a1649c2785c7c.docx";
         File f = new File(filePath, file);
-        // file ´Ù¿î·Îµå ¼³Á¤
+        // file ï¿½Ù¿ï¿½Îµï¿½ ï¿½ï¿½ï¿½ï¿½
         response.setContentType("application/download");
         response.setContentLength((int)f.length());
         file="testNM.docx";
         response.setHeader("Content-disposition", "attachment;filename=\"" + file + "\"");
-        // response °´Ã¼¸¦ ÅëÇØ¼­ ¼­¹ö·ÎºÎÅÍ ÆÄÀÏ ´Ù¿î·Îµå
+        // response ï¿½ï¿½Ã¼ï¿½ï¿½ ï¿½ï¿½ï¿½Ø¼ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Îºï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ù¿ï¿½Îµï¿½
         OutputStream os = response.getOutputStream();
-        // ÆÄÀÏ ÀÔ·Â °´Ã¼ »ý¼º
+        // ï¿½ï¿½ï¿½ï¿½ ï¿½Ô·ï¿½ ï¿½ï¿½Ã¼ ï¿½ï¿½ï¿½ï¿½
         FileInputStream fis = new FileInputStream(f);
         FileCopyUtils.copy(fis, os);
         fis.close();
         os.close();
     }
-    
-    // ´Ù±¹¾î session Ã³¸®
+
+    // ï¿½Ù±ï¿½ï¿½ï¿½ session Ã³ï¿½ï¿½
     @RequestMapping("/local/{lang}")
     public String localLang(@PathVariable String lang
     		, HttpServletRequest request) throws IOException {
     	// todo
         HttpSession session = request.getSession();
 		session.setAttribute("localCountry", lang);
-    	
-		String referer = request.getHeader("Referer"); // Çì´õ¿¡¼­ ÀÌÀü ÆäÀÌÁö¸¦ ÀÐ´Â´Ù.
+
+		String referer = request.getHeader("Referer"); // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ð´Â´ï¿½.
 		return "redirect:"+ referer;
     }
-    
+
+
+    @RequestMapping(value = "/pay", method = RequestMethod.GET)
+	public String papalTest() {
+
+		return "payapalTest";
+	}
 }
